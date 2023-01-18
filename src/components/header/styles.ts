cc76438d5fbbfe9e9e07d95d4b54532a1584653b
Grover Toledo <grovertoledo98@gmail.com>
2023-01-17 20:32:20 -0500

    feature: add tailwind, styled comps and twin-macro dependencies and config

---
+++ src/components/header/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const Container = styled.div`
+  ${tw`bg-black text-white`}
+`;
+
+export default Container;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
+import GlobalStyles from 'components/globalStyles';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <div>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </div>
+);
+
+export default MyApp;
