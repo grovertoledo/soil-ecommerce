0462217f6a18fab6217277266a03b51747e6c479
Grover Toledo <grovertoledo98@gmail.com>
2023-01-18 10:00:47 -0500

    feature: add logo to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import Logo from '../../assets/svg/logo.svg';
 import Container from './styles';
 
-const Header = () => <Container>Header</Container>;
+const Header = () => {
+  const a = 12;
+  return (
+    <Container>
+      <Logo />
+    </Container>
+  );
+};
 
 export default Header;
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 const Container = styled.div`
-  ${tw`bg-black text-white`}
+  ${tw`bg-[#aaa]`}
+  & svg {
+    ${tw`h-[40px] w-auto fill-white`}
+  }
 `;
 
 export default Container;
