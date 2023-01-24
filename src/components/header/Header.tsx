1261dca8323b7fefdab8bd0f4b6bab8fca959621
Grover Toledo <grovertoledo98@gmail.com>
2023-01-23 20:45:12 -0500

    feature: add header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import Logo from '../../assets/svg/logo.svg';
-import Container from './styles';
+import React, { useState } from 'react';
+import LogoBold from '../../assets/svg/logo-bold.svg';
+import * as S from './styles';
 
 const Header = () => {
-  const a = 12;
+  const [isOpened, setIsOpened] = useState(false);
+  const handleModal = () => {
+    setIsOpened(!isOpened);
+  };
   return (
-    <Container>
-      <Logo />
-    </Container>
+    <S.Container>
+      <S.LeftContainer>
+        <LogoBold />
+      </S.LeftContainer>
+      <S.RightContainer>
+        <S.Links className={`${isOpened && 'active'}`}>
+          <li>Inicio</li>
+          <li>Nosotros</li>
+          <li>Productos</li>
+          <li>Contact</li>
+        </S.Links>
+        <button
+          type='button'
+          onClick={handleModal}
+          className={`${isOpened && 'active'}`}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+      </S.RightContainer>
+    </S.Container>
   );
 };
 
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -2,10 +2,65 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 const Container = styled.div`
-  ${tw`bg-[#aaa]`}
-  & svg {
-    ${tw`h-[40px] w-auto fill-white`}
+  ${tw`w-full inline-flex pt-4 pb-3 px-12 bg-[#eee] items-center fixed z-50 justify-between`}
+  top: 0px;
+  @media (min-width: 425px) and (max-width: 767px) {
+    ${tw`px-6`}
+  }
+  @media (max-width: 424px) {
+    ${tw`pt-3 pb-2`}
   }
 `;
-
-export default Container;
+const LeftContainer = styled.div`
+  & svg {
+    ${tw`h-10 fill-black`}
+  }
+`;
+const RightContainer = styled.div`
+  ${tw`flex`}
+  button {
+    ${tw`hover:cursor-pointer`}
+    & span {
+      ${tw`rounded-full`}
+      width: 50px;
+      height: 5px;
+      background-color: black;
+      display: block;
+      margin: 8px 0;
+      transition: all 0.3s ease-in-out;
+      -webkit-transition: all 0.3s ease-in-out;
+      -o-transition: all 0.3s ease-in-out;
+    }
+    &.active span:nth-child(1) {
+      -webkit-transform: translateY(13px) rotate(45deg);
+      -ms-transform: translateY(13px) rotate(45deg);
+      -o-transform: translateY(13px) rotate(45deg);
+      transform: translateY(13px) rotate(45deg);
+    }
+    &.active span:nth-child(2) {
+      opacity: 0;
+    }
+    &.active span:nth-child(3) {
+      -webkit-transform: translateY(-13px) rotate(-45deg);
+      -ms-transform: translateY(-13px) rotate(-45deg);
+      -o-transform: translateY(-13px) rotate(-45deg);
+      transform: translateY(-13px) rotate(-45deg);
+    }
+    @media (min-width: 1024px) {
+      ${tw`hidden`}
+    }
+  }
+`;
+const Links = styled.ul`
+  ${tw`flex`}
+  @media (max-width: 1023px) {
+    /* -right-1/2 */
+    ${tw`flex-col justify-center items-center absolute bg-gray-700 text-white w-1/2 h-screen top-0 -z-10`}
+    right: -50%;
+    transition: all 0.5s ease;
+    &.active {
+      right: 0;
+    }
+  }
+`;
+export { Container, LeftContainer, RightContainer, Links };
